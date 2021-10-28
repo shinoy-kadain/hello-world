a94efe6e6f2bbe2a1772464e64fc6353a4ee6a07
shinoy-kadain <shinoy123.kk@gmail.com>
2021-10-28 17:47:05 +0530

    Helloworld added

---
+++ src/main/java/com/demo/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.demo.helloworld;
+
+
+public class HelloWorld {
+
+  public static void main(final String[] args) {
+
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
+  }
+
+}
