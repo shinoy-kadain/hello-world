b25a0a904a5afe7bc2fb3db13588e82228c8810e
shinoy-kadain <shinoy123.kk@gmail.com>
2021-10-28 16:29:25 +0530

    commit initial java programme

---
+++ src/main/java/com/demo/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.demo.helloworld;
+
+
+public class HelloWorld {
+
+  public static void main(final String[] args) {
+
+    System.out.println("Hello, World! CI/CD Pipeline programme demo" );
+  }
+
+}
